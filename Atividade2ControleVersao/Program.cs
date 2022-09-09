16305d0064d8ea273da8555422c4d174c56398ed
vntgape <gabriel.pereira@venturus.org.br>
2022-09-08 22:48:44 -0300

    first commit

---
+++ Atividade2ControleVersao/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Atividade2ControleVersao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
